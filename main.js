4c1ceadcec082a2d20ce62e44315a2b472a5d578
Urszula <u.postolowicz@gmail.com>
2021-01-26 12:50:54 +0100

    Add task from input

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
+const addBtn = document.querySelector(".add_task_btn");
+const input = document.querySelector(".input");
 const tasksList = document.querySelector(".tasks");
 
-const tasks = [
-  {
-    id: 1,
-    task: "Do the laundry",
-  },
-  {
-    id: 2,
-    task: "Walk the dog",
-  },
-  {
-    id: 3,
-    task: "Study math",
-  },
-];
+const tasks = ["Do the laundry", "Walk the dog", "Study math"];
 
 for (i = 0; i < tasks.length; i++) {
   const task = document.createElement("li");
-  task.textContent = tasks[i].task;
+  task.textContent = tasks[i];
+  task.classList.add("task");
+  task.id = i;
   tasksList.appendChild(task);
 }
 
-console.log(tasksList);
+const addTask = (e) => {
+  e.preventDefault();
+  const newTask = input.value;
+  tasks.push(newTask);
+  const task = document.createElement("li");
+  task.textContent = newTask;
+  task.classList.add("task");
+  task.id = i;
+  tasksList.appendChild(task);
+  input.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
