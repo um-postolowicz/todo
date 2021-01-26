f4aba8cc6827e252ef70739d4007fa31915111be
Urszula <u.postolowicz@gmail.com>
2021-01-26 13:01:44 +0100

    Add item's ID dynamically

--- main.js
+++ main.js
@@ -2,24 +2,18 @@ const addBtn = document.querySelector(".add_task_btn");
 const input = document.querySelector(".input");
 const tasksList = document.querySelector(".tasks");
 
-const tasks = ["Do the laundry", "Walk the dog", "Study math"];
-
-for (i = 0; i < tasks.length; i++) {
-  const task = document.createElement("li");
-  task.textContent = tasks[i];
-  task.classList.add("task");
-  task.id = i;
-  tasksList.appendChild(task);
-}
+const tasks = [];
+let index = 0;
 
 const addTask = (e) => {
+  index = index + 1;
   e.preventDefault();
   const newTask = input.value;
   tasks.push(newTask);
   const task = document.createElement("li");
   task.textContent = newTask;
   task.classList.add("task");
-  task.id = i;
+  task.id = index;
   tasksList.appendChild(task);
   input.value = "";
 };
